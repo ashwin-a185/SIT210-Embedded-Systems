8d4c311632769add68c1700221b8a5485e7c8c20
ashwin-a185 <ashwin.am185@gmail.com>
2026-03-14 19:18:34 +1100

    Update Task1.1Lights_ON.ino

--- Task1.1P/Task1.1Lights_ON.ino
+++ Task1.1P/Task1.1Lights_ON.ino
@@ -1,1 +1,35 @@
+// Linda's home lights program
+// Enables pressed button to turn on two led lights; one for 30 seconds and the other for 60 seconds.
+
+// Pin definitions 
+const int porch_led= 9; 
+const int hall_led= 8; 
+const int user_switch= 2;
+
+
+// Setup function
+void setup() {
+  pinMode(porch_led, OUTPUT);
+  pinMode(hall_led, OUTPUT);
+  pinMode(user_switch, INPUT_PULLUP);
+}
+
+// Looped function
+void loop() {
+if (digitalRead(user_switch) == LOW) {
+    linda_timed_lights();
+
+}
+// Module
+void linda_timed_lights() {
+digitalWrite(porch_light, HIGH);
+digitalWrite(hall_light, HIGH);  
+
+delay(30000);
+digitalWrite(porch_light, LOW);
+
+delay(30000);
+digitalWrite(hall_light, LOW);
+
+}  
 
